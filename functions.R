277746971500df9385c199f9039d75f2fd0e8719
ScienceOlbrich <Michael_Olbrich@gmx.de>
2022-10-04 16:32:09 +0200

    - updated chromosomal range selection - adjusted sIDs to reflect binning and used references - save and load on startup available for imported data - included dot, rect, segment, and fancy plots

--- functions.R
+++ functions.R
@@ -29,6 +29,7 @@ movingAverage <- function(input.vec, window.size = 20) {
 }
 
 
+
 #' Import cnv-profile tables
 #'
 #' For a list of cnv-profiles tables the function will import them into a list
@@ -62,7 +63,8 @@ importData <- function(file.path, chrom.sizes, file.tag = ".data.", ratio.column
                                                               "chrX","chrY"))) %>%
             dplyr::arrange(., chrom) %>%
             # create additional column with ratios rescaled to I=[-1,1]
-            dplyr::mutate(., sID = unlist(strsplit(f.idx, "_", fixed=TRUE))[1]) %>%
+            #dplyr::mutate(., sID = unlist(strsplit(f.idx, "_", fixed=TRUE))[1]) %>%
+            dplyr::mutate(., sID = unlist(strsplit(f.idx, ".", fixed=TRUE))[1]) %>%
             dplyr::mutate(., ref = unlist(strsplit(f.idx, "_", fixed=TRUE))[2]) %>%
             dplyr::mutate(., binSize = unlist(strsplit(f.idx, "_", fixed=TRUE))[3]) %>%
             dplyr::mutate(., scaled.ratio = setInterval(get(ratio.column),output.range = c(-1,1),input.midvalue = 1)) %>%
@@ -71,8 +73,7 @@ importData <- function(file.path, chrom.sizes, file.tag = ".data.", ratio.column
             dplyr::mutate(., color.ratio = factor(ifelse(get(ratio.column) < 1, "loss","gain"), levels = c("loss","gain"))) %>%
             dplyr::mutate(., movAvg = unlist(aggregate(log10.ratio ~ chrom + sID, data=., movingAverage)$log10.ratio)) %>%
             dplyr::mutate(., loss.ratio = ifelse(log10.ratio < 0, log10.ratio, 0)) %>%
-            dplyr::mutate(., gain.ratio = ifelse(log10.ratio >= 0, log10.ratio, 0)) %>%
-            dplyr::left_join(., chrom.sizes, by = "chrom")
+            dplyr::mutate(., gain.ratio = ifelse(log10.ratio >= 0, log10.ratio, 0))
     }
 
     return(data_storage)
@@ -123,56 +124,94 @@ setInterval <- function(input.vec, output.range = c(-1,1), input.midvalue = NULL
 #' @param cnv_data Output of SMURFSeq protocol but pre-processed (ToDo!)
 #' @param p.type Select between Segment and Bar-plot with 'seg' or 'rect'
 #' @param v.type Select value column in 'cnv_data' by name. Defaults to 'ratio'
-plotCNV <- function(cnv_data, p.type = "rect", v.type = "log10.ratio", c.min, c.max) {
-
+#' @param c.min For chromosome range selection, this is the starting position.
+#' @param c.max For chromosome range selection, this is the end-position.
+plotCNV <- function(cnv_data, ref_aes, p.type = "rect", v.type = "log10.ratio", c.min, c.max) {
+    ## color-palette
     copal <- pals::tableau20(20)[c(1,3,5,7,9,11,13,15,17,19)]
 
+    ## filter aesthetics df
+    frame.aes <- ref_aes[[eval(app.state$ref_select)]] %>%
+        dplyr::filter(., chrom %in% unique(cnv_data$chrom))
+
+    ## get min/max lengths of the plots right
     if(missing(c.min) || missing(c.max)) {
         c.min <- 0
-        c.max <- cnv_data$clength
-        #c.max <- max(cnv_data$clength)
+        c.max <- frame.aes$clength
     }
 
-    if( p.type == 'rect' ) {
+    if( p.type == 'point' ) {
 
-        p.obj <- ggplot(cnv_data, aes(ymin = 0,color=sID)) +
-            geom_rect(aes(xmin = chrompos, xmax = chromendpos,
-                          ymax = get(v.type), fill = sID,
-                          group = sID, alpha = 0.25)) +
-            ## plot chromosome lengths to get panel widths right
-            geom_segment(aes(x = c.min, y = 0, xend = c.max, yend = 0,
-                             group=chrom),
+        p.obj <- ggplot() +
+            geom_point(data = cnv_data,
+                       aes(x = (chromendpos + chrompos)/2, y = get(v.type))) +
+            ## Split by chromosome
+            facet_wrap(facets=vars(chrom), nrow=6, ncol=4, scales='free_x') +
+            ## Include center-line to get the widths right
+            geom_segment(data = frame.aes, aes(x = c.min, y = 0, xend = c.max, yend = 0,
+                                              group=chrom),
                          linetype = "dashed", size = 0.2,
                          alpha = 0.5, colour = "black") +
-            #facet_grid(cols=vars(chrom), scales='free_x', space='free_x') +
-            facet_wrap(facets=vars(chrom), nrow=6, ncol=4, scales='free_x') +
+            ## draw rectangle for centromere dimensions
+            geom_rect(data=frame.aes, aes(xmin = centroStart, xmax = centroEnd,
+                                          ymin = min(cnv_data[,get(v.type)]),
+                                          ymax = max(cnv_data[,get(v.type)])),
+                      alpha = 0.2, fill = "blue") +
+            ## Aesthetic shenanigance
             theme_cnv() +
             scale_color_manual(values=copal) +
             labs(x="HG38 5K-bins", y="Ratio of bin counts to genomic average",
                  name="Covariate")
 
-    } else if( p.type == 'seg' ) {
+    } else if( p.type == 'rect' ) {
 
-        line.pos <- round(cnv_data$chromendpos -
-                              ((cnv_data$chromendpos - cnv_data$chrompos) / 2))
-
-        p.obj <- ggplot(cnv_data, aes(ymin = 0,color=color.ratio)) +
-            ## plot the bin values
-            geom_point(aes(x = chrompos, y = get(v.type),
-                             fill = sID, group = sID, alpha = 0.25),size=1) +
-            # geom_segment(aes(x = chrompos, y = get(v.type), xend = chromendpos,
-            #                  yend = get(v.type),
-            #                  fill = sID, group = sID, alpha = 0.25),size=1) +
-            # draw moving average
-            geom_path(aes(x = line.pos, y = movAvg, group=1)) +
-            ## plot chromosome lengths to get panel widths right
-            geom_segment(aes(x = c.min, y = 0, xend = c.max, yend = 0,
-                             group=chrom),
+        p.obj <- ggplot() +
+            # geom_point(data = cnv_data,
+            #            aes(x = (chromendpos + chrompos)/2, y = get(v.type))) +
+            geom_rect(data = cnv_data,
+                      aes(xmin = chrompos, xmax = chromendpos,
+                          ymin = 0, ymax = get(v.type),
+                          colour = sID, group = sID, alpha = 0.25), fill="white", size=0.1) +
+            ## Split by chromosome
+            facet_wrap(facets=vars(chrom), nrow=6, ncol=4, scales='free_x') +
+            ## Include center-line to get the widths right
+            geom_segment(data = frame.aes, aes(x = c.min, y = 0, xend = c.max, yend = 0,
+                                               group=chrom),
                          linetype = "dashed", size = 0.2,
                          alpha = 0.5, colour = "black") +
-            ## split by chromosome to produce panels
-            #facet_grid(cols=vars(chrom), scales='free_x', space='free_x') +
+            ## draw rectangle for centromere dimensions
+            geom_rect(data=frame.aes, aes(xmin = centroStart, xmax = centroEnd,
+                                          ymin = min(cnv_data[,get(v.type)]),
+                                          ymax = max(cnv_data[,get(v.type)])),
+                      alpha = 0.2, fill = "blue") +
+            ## Aesthetic shenanigance
+            theme_cnv() +
+            scale_color_manual(values=copal) +
+            labs(x="HG38 5K-bins", y="Ratio of bin counts to genomic average",
+                 name="Covariate")
+
+
+    } else if( p.type == 'seg' ) {
+
+        p.obj <- ggplot() +
+
+            geom_segment(data = cnv_data,
+                         aes(x = chrompos, y = get(v.type), xend = chromendpos,
+                             yend = get(v.type),
+                             colour = sID, group = sID, alpha = 0.5),size=1) +
+            ## Split by chromosome
             facet_wrap(facets=vars(chrom), nrow=6, ncol=4, scales='free_x') +
+            ## Include center-line to get the widths right
+            geom_segment(data = frame.aes, aes(x = c.min, y = 0, xend = c.max, yend = 0,
+                                               group=chrom),
+                         linetype = "dashed", size = 0.2,
+                         alpha = 0.5, colour = "black") +
+            ## draw rectangle for centromere dimensions
+            geom_rect(data=frame.aes, aes(xmin = centroStart, xmax = centroEnd,
+                                          ymin = min(cnv_data[,get(v.type)]),
+                                          ymax = max(cnv_data[,get(v.type)])),
+                      alpha = 0.2, fill = "blue") +
+            ## Aesthetic shenanigance
             theme_cnv() +
             scale_color_manual(values=copal) +
             labs(x="HG38 5K-bins", y="Ratio of bin counts to genomic average",
@@ -180,25 +219,38 @@ plotCNV <- function(cnv_data, p.type = "rect", v.type = "log10.ratio", c.min, c.
 
     } else if( p.type == 'rect_color' ) {
         # Fancy way of doing jack.
-        p.obj <- ggplot(cnv_data, aes(ymin = 0,color=color.ratio)) +
-            # GAIN/LOSS
-            geom_rect(data=cnv_data, aes(xmin = chrompos, xmax = chromendpos,
-                          ymax = get(v.type), alpha = 0.25),
-                       fill="white", size=0.1) +
-            #scale_x_continuous(breaks = round(seq(c.min, c.max, by = c.max/2),1)) +
-            ## plot chromosome lengths to get panel widths right
-            geom_segment(aes(x = c.min, y = 0, xend = c.max, yend = 0,
-                             group=chrom),
+        p.obj <- ggplot() +
+            # geom_point(data = cnv_data,
+            #            aes(x = (chromendpos + chrompos)/2, y = get(v.type))) +
+            geom_rect(data = cnv_data,
+                      aes(xmin = chrompos, xmax = chromendpos,
+                          ymin = 0, ymax = get(v.type),
+                          color=color.ratio, group = sID, alpha = 0.25), fill="white", size=0.1) +
+            ## Split by chromosome
+            facet_wrap(facets=vars(chrom), nrow=6, ncol=4, scales='free_x') +
+
+            # draw moving average
+            geom_path(data = cnv_data, aes(x = round((chromendpos + chrompos)/2), y = movAvg, group=1), color = "purple") +
+
+            ## Include center-line to get the widths right
+            geom_segment(data = frame.aes, aes(x = c.min, y = 0, xend = c.max, yend = 0,
+                                               group=chrom),
                          linetype = "dashed", size = 0.2,
                          alpha = 0.5, colour = "black") +
-            #facet_grid(cols=vars(chrom), scales='free_x', space='free_x') +
-            facet_wrap(facets=vars(chrom), nrow=6, ncol=4, scales='free_x') +
+            ## draw rectangle for centromere dimensions
+            geom_rect(data=frame.aes, aes(xmin = centroStart, xmax = centroEnd,
+                                          ymin = min(cnv_data[,get(v.type)]),
+                                          ymax = max(cnv_data[,get(v.type)])),
+                      alpha = 0.2, fill = "blue") +
+            ## Aesthetic shenanigance
             theme_cnv() +
-            scale_color_manual(values=c( "#3883c2","#660000", "#56B4E9")) +
+            scale_color_manual(values=copal) +
             labs(x="HG38 5K-bins", y="Ratio of bin counts to genomic average",
                  name="Covariate")
     }
+
     return(p.obj)
+
 }
 
 
